bf36b00f8eaf25bb544fa4268e098e2fc6b04178
KimRiun <kimriun27@gmail.com>
2024-07-12 17:21:37 +0900

    feat: 로그인시 아이디, 비밀번호 입력받는 view 구현

--- src/user/UserController.java
+++ src/user/UserController.java
@@ -7,7 +7,19 @@ public class UserController {
     private static final UserService userService = new UserService();
     private static final UserView view = new UserView();
 
-    public int login(String loginId, String wp) {
+    public String[] loginPage() {
+        return view.loginView();
+    }
+
+    public int login(String[] inputs) {
+        // 입력 개수 예외처리
+        if(inputs.length != 2) {
+            return -1;
+        }
+
+        String loginId = inputs[0];
+        String wp = inputs[1];
+
         User user = userService.login(loginId, wp);
 
         if(user == null) {
