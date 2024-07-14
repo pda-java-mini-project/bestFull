2cbc65feeb205e991e74b1618c2c46d39f68f23f
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-07-14 20:58:02 +0900

    feat: user sign up 구현

--- src/user/service/UserService.java
+++ src/user/service/UserService.java
@@ -9,4 +9,15 @@ public class UserService {
     public User login(String loginId, String wp) {
         return userDAO.select(loginId, wp);
     }
+
+    public boolean signup(String userName, String loginId, String wp) {
+        if (userName.isEmpty() || loginId.isEmpty() || wp.isEmpty()) {
+            return false; // 입력값이 유효하지 않을 경우 false 반환
+        }
+
+        int id = userDAO.generateId();
+        User newUser = new User(id, loginId, userName, wp);
+        userDAO.insert(newUser);
+        return true; // 회원가입 성공 시 true 반환
+    }
 }
