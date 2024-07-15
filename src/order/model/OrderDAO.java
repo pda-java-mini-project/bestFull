1d90281cf2cf42a71913d6eb307aa3ed41a1bc34
YaRkyungmin <kkgm94@naver.com>
2024-07-15 10:29:43 +0900

    ✨ feat: 주문 내역 보기, 상세 내역 보기 로직 구현

--- src/order/model/OrderDAO.java
+++ src/order/model/OrderDAO.java
@@ -1,12 +1,32 @@
 package order.model;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class OrderDAO {
     private HashMap<Integer, Order> orderDb = new HashMap<>();
-    private int autoIncrementIndex = 0;
+    private int autoIncrementIndex = 1;
 
     public void insert(Order order) {
+        order.setId(autoIncrementIndex);
         orderDb.put(autoIncrementIndex++, order);
+    }
+
+    public Order[] fetchAll() {
+        return orderDb.values().toArray(new Order[0]);
+    }
+
+    public Order findById(int id) {
+        return orderDb.get(id);
+    }
+
+    public int[] fetchAllId() {
+        Set<Integer> keys = orderDb.keySet();
+        int[] keyArray = new int[keys.size()];
+        int i = 0;
+        for (Integer key : keys) {
+            keyArray[i++] = key;
+        }
+        return keyArray;
     }
 }
